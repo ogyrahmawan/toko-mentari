44a54836c531cc1477ce7c64ebd157e0212a7fd7
ogyrahmawan <ogyrahmawan@gmail.com>
2021-02-22 14:14:02 +0700

    set up serve: 1. install dependencies express, jwt, bcrypt 2. set up unit test 3. set up routing

---
+++ e-commerce-client/src/redux/types.ts
@@ -0,0 +1,9 @@
+export const SET_PRODUCT = "SET_PRODUCT"
+export const LOADING = "LOADING"
+
+export interface Product {
+  name: string,
+  image : string,
+  price: number,
+  CategoryId: number
+}                                                                                                                               
