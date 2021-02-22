8a6d7ec8076fbf2156fb391d9cb0896ca410275a
ogyrahmawan <ogyrahmawan@gmail.com>
2021-02-22 14:27:22 +0700

    add server

---
+++ ecommerce_server/controllers/productController.js
@@ -0,0 +1,90 @@
+const {Product, Category} = require('../models/index')
+
+class ProductController {
+    static async createProduct(req, res, next){
+        try {
+            let category = await Category.findOne({
+                where: {
+                    name: req.body.categoryName
+                }
+            })
+            let obj = {
+                name: req.body.name,
+                image_url: req.body.image_url,
+                price: req.body.price,
+                CategoryId: category.id
+            }
+            let data = await Product.create(obj)
+            res.status(201).json(data)
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async getAllProduct(req, res, next){
+        try {
+            let data = await Product.findAll({
+                include: {
+                    model: Category
+                }
+            })
+            res.status(200).json(data)
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async getProductById(req, res, next){
+        try {
+            let id = req.params.id
+            let data = await Product.findOne({
+                where: {
+                    id
+                }
+            })
+            res.status(200).json(data)
+        } catch (error) {
+            next(error)
+        }
+    }
+    
+    static async editProduct(req, res, next){
+        try {
+            let category = await Category.findOne({
+                where: {
+                    name: req.body.categoryName
+                }
+            })
+            let obj = {
+                name: req.body.name,
+                image_url: req.body.image_url,
+                price: req.body.price,
+                CategoryId: category.id
+            }
+            let id = req.params.id
+            let data = await Product.update(obj, {
+                where: {
+                    id
+                },
+                returning: true
+            })
+            res.status(200).json(data[1][0])
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async deleteStock(req, res, next){
+        try {
+            let id = req.params.id
+            let data = await Product.destroy({
+                where: {
+                    id
+                }
+            })
+            res.status(200).json({message: `delete success`})
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports = ProductController
---
+++ ecommerce_server/routes/productRoute.js
@@ -0,0 +1,13 @@
+const ProductController = require('../controllers/productController')
+
+const route = require('express').Router()
+const authenctication = require('../middleware/authentication')
+const authorization = require('../middleware/authorization')
+
+route.get('/', ProductController.getAllProduct)
+route.use(authenctication, authorization)
+route.post('/', ProductController.createProduct)
+route.get('/:id', ProductController.getProductById)
+route.put('/:id', ProductController.editProduct)
+route.delete('/:id', ProductController.deleteStock)
+module.exports = route
